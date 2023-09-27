42a4d9bb66ea4836e76ae291bb2a49d691dbe2c3
wins <wins>
2023-09-27 11:50:26 +0300

    color

--- svetofor/ViewController.swift
+++ svetofor/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redCollor: UIView!
+    @IBOutlet weak var yellowCollor: UIView!
+    @IBOutlet weak var greenCollor: UIView!
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        redCollor.alpha = 0.3
+        yellowCollor.alpha = 0.3
+        greenCollor.alpha = 0.3
+        
 
-
+    
 }
-
+    @IBAction func srartButton(_ sender: UIButton) {
+    }
+    
+}
