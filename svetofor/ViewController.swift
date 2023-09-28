5ca1d1d89ffad4ea455ac0b7beb4e722ef8ff213
wins <wins>
2023-09-28 10:49:19 +0300

    the end

--- svetofor/ViewController.swift
+++ svetofor/ViewController.swift
@@ -7,20 +7,72 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLable: UIView!
+    @IBOutlet weak var yellowLabel: UIView!
+    @IBOutlet weak var greenLable: UIView!
     
+    @IBOutlet weak var startButton: UIButton!
     
+    var currentLight = CurrentLight.red
+    let colorON: CGFloat = 1
+    let colorOff: CGFloat = 0.4
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-    
-}
-    @IBAction func srartButton(_ sender: UIButton) {
+        startButton.layer.cornerRadius = 20
         
+        redLable.alpha = colorOff
+        yellowLabel.alpha = colorOff
+        greenLable.alpha = colorOff
         
     }
+    override func viewDidLayoutSubviews() {
+
+        redLable.layer.cornerRadius = redLable.frame.width / 2
+        yellowLabel.layer.cornerRadius = redLable.frame.width / 2
+        greenLable.layer.cornerRadius = redLable.frame.width / 2
+    }
     
-}
+    @IBAction func startButtonPressed() {
+        
+        startButton.setTitle("Next Color", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenLable.alpha = colorOff
+            redLable.alpha = colorON
+            currentLight = .yellow
+            
+        case .yellow:
+            redLable.alpha = colorOff
+            yellowLabel.alpha = colorON
+            currentLight = .green
+            
+        case .green:
+            yellowLabel.alpha = colorOff
+            greenLable.alpha = colorON
+            currentLight = .red
+    
+        }
+        
+        }
+    }
+    
+    
+
+    
+      
+
+    
+
+
+    
+    
+        
